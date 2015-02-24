75debb28236df02c90db76057cfd72cc5ca6e615
hyyungaa <hyyungaa@CSL1WK32.win2k.cs.ust.hk>
2015-02-24 11:31:36 +0800

    This is for Lab assignment #2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World. Comp3021, Lab1");
-
+		
+		//I have successfully cloned my projects, and finished my assignments
+		//Now I want to push my code to the remote repository
 	}
 
 }
